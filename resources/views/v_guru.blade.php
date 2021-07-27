3440ab7ecc3f233b40d7679f9f4ab1d507f4cc0f
dzakyathallah14 <dzakyathallah@gmail.com>
2021-07-27 17:26:50 +0700

    Menambahkan Action Buttons

--- resources/views/v_guru.blade.php
+++ resources/views/v_guru.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h1>Ini Halaman Guru</h1>
 
-    <table class="table table-striped">
+    <table class="table table-bordered text-center">
         <thead>
             <tr>
                 <th>No</th>
@@ -13,6 +13,7 @@
                 <th>Nama</th>
                 <th>Mata Pelajaran</th>
                 <th>Foto</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -23,6 +24,11 @@
                     <td>{{ $data->nama }}</td>
                     <td>{{ $data->mapel }}</td>
                     <td><img src="{{ url('foto/' . $data->foto_guru) }}" alt="foto_guru" width="150"></td>
+                    <td>
+                        <a class="btn btn-primary" href="">Detail</a>
+                        <a class="btn btn-success" href="">Edit</a>
+                        <a class="btn btn-danger" href="">Delete</a>
+                    </td>
                 </tr>
 
             @endforeach
