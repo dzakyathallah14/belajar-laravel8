52215e559761d5a82e1f62b3173f6282f514e010
dzakyathallah14 <dzakyathallah@gmail.com>
2021-07-27 17:21:43 +0700

    Read data dari MySQL Database

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\GuruModel;
 
 class GuruController extends Controller
 {
+    
+    public function __construct()
+    {
+        $this->GuruModel = new GuruModel();
+    }
+    
     public function index()
     {
-        return view('v_guru');
+        $data = [
+            'guru' => $this->GuruModel->alldata(),
+        ];
+        return view('v_guru', $data);
     }
 }
--- resources/views/v_guru.blade.php
+++ resources/views/v_guru.blade.php
@@ -4,4 +4,28 @@
 
 @section('content')
     <h1>Ini Halaman Guru</h1>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>NIP</th>
+                <th>Nama</th>
+                <th>Mata Pelajaran</th>
+                <th>Foto</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($guru as $data)
+                <tr>
+                    <td>{{ $data->id_guru }}</td>
+                    <td>{{ $data->nip }}</td>
+                    <td>{{ $data->nama }}</td>
+                    <td>{{ $data->mapel }}</td>
+                    <td><img src="{{ url('foto/' . $data->foto_guru) }}" alt="foto_guru" width="150"></td>
+                </tr>
+
+            @endforeach
+        </tbody>
+    </table>
 @endsection
